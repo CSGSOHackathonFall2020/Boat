ef73d0cd9fa10f3514b254d0dd9ea3415940cd9c
pkubiojh <jianghao@HaoMBP.local>
2020-10-24 21:27:50 -0500

    commit

--- html/main.js
+++ html/main.js
@@ -1,42 +1,84 @@
-//var, let, const
-// let habibat = 'water';
-// let season = 'winter';
-// let colors ='yellow';
+function sendJSON(){ 
+               
+    let result = document.querySelector('.result'); 
+    const habitatInput = document.querySelector('#habitat');
+    const seasonInput = document.querySelector('#season');
+    const colorInput = document.querySelector('#color'); 
+       
+    // Creating a XHR object 
+    let xhr = new XMLHttpRequest(); 
+    let url = "my.php"; 
 
-const myForm = document.querySelector('#my-form');
-let result = document.querySelector('.result');
-const habitatInput = document.querySelector('#habitat');
-const seasonInput = document.querySelector('#season');
-const colorInput = document.querySelector('#color');
+    // open a connection 
+    xhr.open("POST", url, true); 
 
-let xhr = new XMLHttpRequest();
-let url = "my.php";
+    // Set the request header i.e. which type of content you are sending 
+    xhr.setRequestHeader("Content-Type", "application/json"); 
 
-xhr.open("POST", url, true);
+    // Create a state change callback 
+    xhr.onreadystatechange = function () { 
+        if (xhr.readyState === 4 && xhr.status === 200) { 
 
-xhr.setRequestHeader("Content-Type", "application/json");
+            // Print received data from server 
+            result.innerHTML = this.responseText; 
+
+        } 
+    }; 
+
+    // Converting JSON data to string 
+    var data = JSON.stringify({ "habitatInput": habitatInput.value, 
+                                "seasonInput": seasonInput.value, 
+                                "colorInput": colorInput.value}); 
+
+    // Sending data with the request 
+    xhr.send(data); 
+} 
 
 
-myForm.addEventListener('submit', onSubmit);
+// //var, let, const
+// // let habibat = 'water';
+// // let season = 'winter';
+// // let colors ='yellow';
+
+// const myForm = document.querySelector('#my-form');
+// let result = document.querySelector('.result');
+// const habitatInput = document.querySelector('#habitat');
+// const seasonInput = document.querySelector('#season');
+// const colorInput = document.querySelector('#color');
+
+// let xhr = new XMLHttpRequest();
+// let url = "my.php";
+
+// xhr.open("POST", url, true);
+
+// xhr.setRequestHeader("Content-Type", "application/json");
+
+// xhr.onreadystatechange = function(){
+//     if(xhr.readyState === 4 && xhr.status === 200){
+//         result.innerHTML = this.responseText;
+//     }
+// };
+
+// myForm.addEventListener('submit', onSubmit);
 
 
-function onSubmit(e){
-    e.preventDefault();
-    console.log(habitatInput.value);
-    console.log(seasonInput.value);
-    console.log(colorInput.value);
-}
-
-
-
-
-// let parameter = {
-//     habitat:'water',
-//     season:'winter',
-//     colors:'yellow'
+// function onSubmit(e){
+//     e.preventDefault();
+//     console.log(habitatInput.value);
+//     console.log(seasonInput.value);
+//     console.log(colorInput.value);
 // }
-//console.log(parameter.season);
 
-//conver to jason and send to server
-// const parameterJSON = JSON.stringify(parameter);
-//console.log(parameterJSON);
+
+
+
+// // let parameter = {
+// //     habitat:'water',
+// //     season:'winter',
+// //     colors:'yellow'
+// // }
+// //console.log(parameter.season);
+
+// //conver to jason and send to server
+// // const parameterJSON = JSON.stringify(parameter);
+// //console.log(parameterJSON);
