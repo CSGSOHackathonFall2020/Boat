1b013bc4c72ccf2cd7dd81321a82d6cc9edd81d5
pkubiojh <jianghao@HaoMBP.local>
2020-10-24 20:13:45 -0500

    commit

--- html/main.js
+++ html/main.js
@@ -1,3 +1,14 @@
 //var, let, const
+// let habibat = 'water';
+// let season = 'winter';
+// let colors ='yellow';
 
-console.log('Hello World');
+let parameter = {
+    habibat:'water',
+    season:'winter',
+    colors:'yellow'
+}
+console.log(parameter.season);
+
+//conver to jason and send to server
+const todoJSON = JSON.stringify
