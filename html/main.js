b75a2bdc801da93a65bc98c30c4b4f726c30f7cd
pkubiojh <jianghao@HaoMBP.local>
2020-10-24 20:55:10 -0500

    commit

--- html/main.js
+++ html/main.js
@@ -3,12 +3,27 @@
 // let season = 'winter';
 // let colors ='yellow';
 
+const myForm = document.querySelector('#my-form');
+const habitatInput = document.querySelector('#habitat');
+const seasonInput = document.querySelector('#season');
+const colorInput = document.querySelector('#colors');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    console.log(habitatInput.value);
+}
+
+
+
 let parameter = {
-    habibat:'water',
+    habitat:'water',
     season:'winter',
     colors:'yellow'
 }
-console.log(parameter.season);
+//console.log(parameter.season);
 
 //conver to jason and send to server
-const todoJSON = JSON.stringify
+const parameterJSON = JSON.stringify(parameter);
+//console.log(parameterJSON);
